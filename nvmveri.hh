5e9d614b87dce50a54da7f9507b5d74ec6d89fef
Sihang Liu <sihangliu@virginia.edu>
2018-02-18 15:10:37 -0500

    int

--- nvmveri.hh
+++ nvmveri.hh
@@ -1,2 +1,35 @@
+#ifndef NVMVERI_HH
+#define NVMVERI_HH
 // Libary for verification
 
+#include <cstdlib>
+#include <cstring>
+#include <sys/shm.h>
+#include <mqueue.h>
+#include <pthread.h>
+
+
+struct State;
+
+struct NVMVeri {
+	bool initVeri();
+	bool readMetadata();
+	bool writeMetadata();	
+		
+	NVMVeri();
+};
+
+struct Metadata {
+	State state;
+	// ...
+};
+
+
+struct State {
+	enum StateVal {INIT, COMMIT};
+	char StateChar[] = {"init", "commit"};
+	void tranState();
+};
+
+
+#endif
