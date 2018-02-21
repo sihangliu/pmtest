881adcacb87eae7d8d88310a8d5906659297204a
Sihang Liu <sihangliu@virginia.edu>
2018-02-20 21:16:22 -0500

    function name

---
+++ nvmveri.cc
@@ -0,0 +1,37 @@
+#include "nvmveri.hh"
+
+static void* NVMVeri::VeriWorker(void* a) {
+	tid_t tid = *((int*) a);	
+	
+}
+
+static void* NVMVeri::VeriMaster(void* a) {
+	
+}
+
+bool NVMVeri::init() {
+	// create master
+	pthread_t MasterThread;
+	pthread_create(&MasterThread, NULL, &VeriMaster, (void*)0);
+	
+	// create worker
+	for (int i = 0; i < MAX_THREAD_POOL_SIZE) {
+		pthread_t VeriThread;
+		tid_t VeriWorkerID = i;
+		VeriWorkerStateMap[VeriWorkerID] = IDLE;
+		VeriWorkerLock[i] = PTHREAD_MUTEX_INITIALIZER;
+		pthread_create(&VeriThread, NULL, &VeriWorker, VeriWorkerID);
+	}
+}
+
+bool NVMVeri::readMetadata() {
+		
+}
+
+bool NVMVeri::writeMetadata() {
+	
+}
+
+bool NVMVeri::assignTask(tid_t VeriWorkerID) {
+	
+}
--- nvmveri.hh
+++ nvmveri.hh
@@ -7,15 +7,47 @@
 #include <sys/shm.h>
 #include <mqueue.h>
 #include <pthread.h>
+#include <queue>
+using namespace std::queue;
+#include <unordered_map>
+using namespace std::unordered_map;
+#include <string>
+using namespace std::string;
+
+#define MAX_THREAD_POOL_SIZE 1
+typedef tid_t unsigned int;
 
 
 struct State;
+enum VeriWorkerState {IDLE, BUSY};
+enum ResultType {PASS, FAIL};
 
 struct NVMVeri {
+	/* Initialize each worker */
 	bool initVeri();
+	/* Terminate each worker
+	 * Check state has become IDLE
+	 * Join all worker/master threads
+	 */
+	bool termVeri();
+	/* Read data passed from the master thread */
 	bool readMetadata();
+	/* Write data passed from the master thread */
 	bool writeMetadata();	
-		
+
+	/* Worker function. 
+	 * If state is IDLE, busy waiting
+	 * If state is BUSY, read metadata and verify. 
+	 * When verification completes, send result to master
+	 */
+	static void* VeriMaster(void*);
+	static void* VeriWorker(void*);
+	static VeriWorkerState VeriWorkerStateMap[MAX_THREAD_POOL_SIZE];
+	static pthread_mutex_t VeriWorkerLock[MAX_THREAD_POOL_SIZE];
+
+	bool assignTask(tid_t);	
+	
+	// call init
 	NVMVeri();
 };
 
@@ -24,6 +56,9 @@ struct Metadata {
 	// ...
 };
 
+struct VeriResult {
+	ResultType result;
+}
 
 struct State {
 	enum StateVal {INIT, COMMIT};
