64b12e47a4c8806b1bd069ffb95ddf541246e6e3
Sihang Liu <sihangliu@virginia.edu>
2018-05-01 21:31:16 -0400

    kernel

---
+++ include/knvmveri.h
@@ -0,0 +1,25 @@
+#include <asm/types.h>
+#include <sys/socket.h>
+#include <linux/netlink.h>
+
+#define MAX_PAYLOAD 1024
+/* Protocol family, consistent in both kernel prog and user prog. */
+#define MYPROTO NETLINK_USERSOCK
+/* Multicast group, consistent in both kernel prog and user prog. */
+#define MYGRP 21
+
+
+struct NetlinkMetadata {
+	struct sockaddr_nl src_addr;
+	struct sockaddr_nl dest_addr;
+	struct nlmsghdr *nlh = NULL;
+	struct iovec iov;
+	int sock_fd;
+};
+
+struct kNVMVeri {
+	struct NetlinkMetadata connection;
+	
+	bool initVeri();
+
+};
---
+++ src/nvmveri_socket.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <linux/netlink.h>
+#include <unistd.h>
+
+/* Protocol family, consistent in both kernel prog and user prog. */
+#define MYPROTO NETLINK_USERSOCK
+/* Multicast group, consistent in both kernel prog and user prog. */
+#define MYMGRP 21
+
+#define MAX_MSG_LENGTH 65536
+
+int open_netlink(void)
+{
+    int sock;
+    struct sockaddr_nl addr;
+    int group = MYMGRP;
+
+    sock = socket(AF_NETLINK, SOCK_RAW, MYPROTO);
+    if (sock < 0) {
+        printf("sock < 0.\n");
+        return sock;
+    }
+
+    memset((void *) &addr, 0, sizeof(addr));
+    addr.nl_family = AF_NETLINK;
+    addr.nl_pid = getpid();
+
+    if (bind(sock, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
+        printf("bind < 0.\n");
+        return -1;
+    }
+
+    if (setsockopt(sock, 270, NETLINK_ADD_MEMBERSHIP, &group, sizeof(group)) < 0) {
+        printf("setsockopt < 0\n");
+        return -1;
+    }
+
+    return sock;
+}
+
+void read_event(int sock)
+{
+    struct sockaddr_nl nladdr;
+    struct msghdr msg;
+    struct iovec iov;
+    char buffer[MAX_MSG_LENGTH];
+    int ret;
+
+    iov.iov_base = (void *) buffer;
+    iov.iov_len = sizeof(buffer);
+    msg.msg_name = (void *) &(nladdr);
+    msg.msg_namelen = sizeof(nladdr);
+    msg.msg_iov = &iov;
+    msg.msg_iovlen = 1;
+
+    printf("Ok, listening.\n");
+    ret = recvmsg(sock, &msg, 0);
+    if (ret < 0)
+        printf("ret < 0.\n");
+    else
+        printf("Received message payload: %s\n", NLMSG_DATA((struct nlmsghdr *) &buffer));
+}
+
+int main(int argc, char *argv[])
+{
+    int nls;
+
+    nls = open_netlink();
+    if (nls < 0)
+        return nls;
+
+    while (1) {
+		num_packets = read_event(nls);
+
+
+		if (num_packets == 0) break;
+		
+	}
+
+    return 0;
+}
+
