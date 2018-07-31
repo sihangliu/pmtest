fd3e2c1f2315b0cc46ee1d153eb4b2c42c8256cc
sihang liu <sl4jg@virginia.edu>
2018-07-31 19:21:32 -0400

    latnecy

--- include/latency.h
+++ include/latency.h
@@ -1,30 +1,33 @@
+#ifndef __LATENCY_H__
+#define __LATENCY_H__
+
 #include <sys/time.h>
 #include <stdint.h>
 
 #define FREQ 2.5f
 
-static inline uint64_t rdtsc() {
-	uint32_t hi, lo;
-	asm volatile("rdtscp\n"
-	             "movl %%edx, %0\n"
-	             "movl %%eax, %1\n"
-	             "cpuid"
-               : "=r" (hi), "=r" (lo) : : "%rax", "%rbx", "%rcx", "%rdx");
-    return (((uint64_t)hi) << 32) | (uint64_t)lo;
-}
-
-
 // wait for nanoseconds
-void inline waitFor(unsigned int duration) 
-{
-	uint64_t start, end;
-	start = rdtsc();
-	do {
-		end = rdtsc();
-	} while ((end - start) / FREQ < duration);
-}
+#define pmLatency(duration) \
+	uint64_t start, end;   \
+	uint32_t hi, lo; \
+	asm volatile("rdtscp\n" \
+	             "movl %%edx, %0\n" \
+	             "movl %%eax, %1\n" \
+	             "cpuid"            \
+               : "=r" (hi), "=r" (lo) : : "%rax", "%rbx", "%rcx", "%rdx"); \
+    start = (((uint64_t)hi) << 32) | (uint64_t)lo; \
+	do { \
+		asm volatile("rdtscp\n" \
+					 "movl %%edx, %0\n" \
+					 "movl %%eax, %1\n" \
+					 "cpuid"           \
+				   : "=r" (hi), "=r" (lo) : : "%rax", "%rbx", "%rcx", "%rdx"); \
+		end = (((uint64_t)hi) << 32) | (uint64_t)lo; \
+	} while ((end - start) / FREQ < duration); 
 
 // extra latency for NVM
-#define STT_WRITE_LATENCY waitFor(30)
-#define PCM_WRITE_LATENCY waitFor(120)
-#define PCM_READ_LATENCY waitFor(25)
+#define STT_WRITE_LATENCY 30
+#define PCM_WRITE_LATENCY 120
+#define PCM_READ_LATENCY 25
+
+#endif
