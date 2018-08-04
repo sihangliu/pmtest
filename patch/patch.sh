c41e289b8ef5bdcc087aaf99cdde7d576d72add6
Sihang Liu <sihangliu@virginia.edu>
2018-08-03 21:32:08 -0400

    update patch script

--- patch/patch.sh
+++ patch/patch.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-if [ ${1:-foo} == "-R" ]; then
-	cp $3 $2
-	cd $2
-	git apply -R $3
-	rm $3
+if [ $1 == "-R" ]; then
+	cp $(readlink -f $3) $(readlink -f $2)
+	cd $(readlink -f $2)
+	git apply -R $(basename $3)
+	rm $(basename $3)
 else
-	cp $2 $1
-	cd $1
-	git apply $2
-	rm $2
+	cp $(readlink -f $2) $(readlink -f $1)
+	cd $(readlink -f $1)
+	git apply $(basename $2)
+	rm $(basename $2)
 fi
